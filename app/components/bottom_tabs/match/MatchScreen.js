5956dd6e2b0b58e88b2e5b5a1fa9d1a3fe902e04
SebySurfer <sebysurfer@gmail.com>
2024-10-17 12:04:51 -0600

    FIxed Xcode Setup
    Also included main components

---
+++ app/components/bottom_tabs/match/MatchScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const MatchScreen = (props) => {
+
+    return(
+        <View>
+        <Text> Match Screen </Text>
+    </View>
+
+    )
+
+}
+
+export default MatchScreen;
---
+++ app/components/bottom_tabs/profile/ProfileScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const ProfileScreen = () => {
+    return (
+        <View> 
+            <Text>Profile Screen </Text>
+
+        </View>
+    )
+
+}
+
+export default ProfileScreen;
+
