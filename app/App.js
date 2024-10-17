d70508258af4ce9dc73c9f3be8a9f7c8bf7f0bd7
SebySurfer <sebysurfer@gmail.com>
2024-10-17 15:01:29 -0600

    Final point of problems

--- app/App.js
+++ app/App.js
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'; 
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//import { createDrawerNavigator } from '@react-navigation/drawer';
+
 
 
 export default function App() {
