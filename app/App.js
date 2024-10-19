5d43eee6062ac8dddf5a845d6a395a8da309271f
SebySurfer <sebysurfer@gmail.com>
2024-10-19 10:49:24 -0600

    Tabs working

--- app/App.js
+++ app/App.js
@@ -1,5 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import MatchScreen from './screens/bottom_tabs/match/MatchScreen.js';
+import ProfileScreen from './screens/bottom_tabs/profile/ProfileScreen.js';
+
+
 import { NavigationContainer } from '@react-navigation/native'; 
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -7,13 +12,27 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 
 
 
+//const Stack = createStackNavigator();
+const BottomTab = createBottomTabNavigator();
+//const Drawer = createDrawerNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+
+      <BottomTab.Navigator>
+      
+        <BottomTab.Screen name="Profile" component={ProfileScreen}/> 
+        <BottomTab.Screen name="Match" component={MatchScreen}/> 
+        
+
+      </BottomTab.Navigator>
+
+
+
+    </NavigationContainer>
+    
   );
 }
 
