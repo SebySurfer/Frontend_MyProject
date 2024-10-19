71a749460745a51efd58551d6a9abf56648cf95d
SebySurfer <sebysurfer@gmail.com>
2024-10-19 16:56:24 -0600

    Comparison of Failure

--- app/screens/settings/AccountScreen.js
+++ app/screens/settings/AccountScreen.js
@@ -1,3 +1,4 @@
+import { Link } from 'expo-router';
 import React from 'react';
 import {View, Text, Button} from 'react-native';
 
@@ -5,8 +6,11 @@ const AccountScreen = ({navigation}) =>{
     return(
         <View>
             <Text> Account Screen</Text>
+            <Link href={'./AboutScreen'}>
+            <Text> Go to About by Expo Router</Text>
+            </Link>
             <Button
-            title="Go to About"
+            title="Go to About by normal react-native component"
             onPress={() => navigation.navigate('About')}
         />
 
