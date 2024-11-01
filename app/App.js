10dc9495865d772aa301c5d1a4755ed1bcf89d54
SebySurfer <sebysurfer@gmail.com>
2024-11-01 17:18:54 -0600

    Main setup

--- app/App.js
+++ app/App.js
@@ -1,80 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-//use Linking
-//createMaterialTopNavvigator
 
-import { NavigationContainer } from '@react-navigation/native'; 
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import SessionNavigator from './navigation/SessionNavigator.js';
 
-
-import AboutScreen from './screens/settings/AboutScreen.js';
-import AccountScreen from './screens/settings/AccountScreen.js';
-import ProfileScreen from './screens/bottom_tabs/ProfileScreen.js'
-import MatchScreen from './screens/bottom_tabs/MatchScreen.js'
-import CustomDrawerContent from './screens/settings/CustomDrawerContent.js';
-
-
-
-const Stack = createStackNavigator();
-const BottomTab = createBottomTabNavigator();
-const Drawer = createDrawerNavigator();
-
-const TabNavigator = () =>{
-  return(
-    <NavigationContainer>
-    <BottomTab.Navigator>
-      <BottomTab.Screen name="Profile" component={ProfileScreen}/> 
-      <BottomTab.Screen name="Match" component={MatchScreen}/> 
-    </BottomTab.Navigator>
-  </NavigationContainer>
-
-  )
-}
-
-const DrawerNavigator = () =>{
-  return(
-    <NavigationContainer>
-    <Drawer.Navigator>
-      <Drawer.Screen name="Account" component={AccountScreen}/>
-      <Drawer.Screen name="About" component={AboutScreen}/>
-    </Drawer.Navigator>
-    </NavigationContainer>
-
-  )
-}
-
-//Test 
-const StackNavigator = () => {
-  return(
-    <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Account" component={AccountScreen}/>
-      <Stack.Screen name="About" component={AboutScreen}/>
-
-    </Stack.Navigator>
-    </NavigationContainer>
-
-
-  )
-}
-
-/*
-
-
- */
 
 export default function App() {
   return (
-    <>
-    <DrawerNavigator/>
-    
 
-    </>
-    
-    
-    
+    <SessionNavigator/>
+  
   );
 }
 
---
+++ app/screens/session/tabs/MatchScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function MatchScreen() {
+  return (
+    <View>
+      <Text>MatchScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
