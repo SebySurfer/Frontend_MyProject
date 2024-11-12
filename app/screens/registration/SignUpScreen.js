235e8145c26baa08b5f9404c1bfebd934023095c
SebySurfer <sebysurfer@gmail.com>
2024-11-12 11:39:43 -0600

    Actions work

--- app/screens/registration/SignUpScreen.js
+++ app/screens/registration/SignUpScreen.js
@@ -1,12 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ImageBackground, Image, Button} from 'react-native'
 import React from 'react'
 
-export default function SignUpScreen() {
+export default function SignUpScreen({navigation}) {
   return (
-    <View>
-      <Text>SignUpScreen</Text>
+    <ImageBackground
+        source={require('../../assets/Background.png')}
+        style= {styles.ImageStyle}
+    >
+    <View style={styles.logoContainer}>
+        <Image source={require('../../assets/dating-site-icon.png')} style={styles.Icon}></Image>
+        <Text> Proyecto de la Amistad </Text>
     </View>
+
+    <View style={styles.Button}>
+    <Button  
+    title='Lets Get Started'
+    onPress={ () => navigation.navigate('Forms')}/>
+    </View>
+    </ImageBackground>
+
+
+    
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    ImageStyle:{
+        
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+        
+    },
+
+    Button:{
+        height: 70, 
+        backgroundColor: '#75f1c1',
+
+    }, 
+    LogoContainer:{
+        position: 'absolute', 
+        alignItems: 'center'
+
+    }, 
+    Icon:{
+        height: 100, 
+        width: 100
+    }
+
+
+})
