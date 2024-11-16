51f433b1336dad00ad5cae52496a5a037f51b77c
SebySurfer <sebysurfer@gmail.com>
2024-11-16 13:23:11 -0600

    Bottom Tab Icons
    Success

--- app/navigation/SessionNavigator.js
+++ app/navigation/SessionNavigator.js
@@ -6,6 +6,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 import FilterScreen from '../screens/session/tabs/FilterScreen';
 import MatchScreen from '../screens/session/tabs/MatchScreen';
@@ -19,8 +20,29 @@ const Drawer = createDrawerNavigator();
 
 function TabNavigator(){
     return(
-        <Tab.Navigator>
-            <Tab.Screen name='Filters' component={FilterScreen} options={{ headerShown: false, headerBlurEffect: 'regular',  }}/>
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+  
+            if (route.name === 'Filters') {
+              iconName = focused ? 'filter' : 'filter-outline';
+            } else if (route.name === 'Profiles') {
+              iconName = focused ? 'people' : 'people-outline';
+            }
+  
+            // Return the Ionicon component
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#bd8bf0', // Primary color
+          tabBarInactiveTintColor: '#183371', // Text color
+          headerShown: false,
+        })}
+        
+          
+          
+        >
+            <Tab.Screen name='Filters' component={FilterScreen} options={{ headerShown: false, headerBlurEffect: 'regular'}}/>
             <Tab.Screen name='Profiles' component={MatchScreen} options={{ headerShown: false }}/>
 
         </Tab.Navigator>
