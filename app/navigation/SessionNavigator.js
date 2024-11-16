eba0434c2f49a4160b2cac9ad73a997659b084ac
SebySurfer <sebysurfer@gmail.com>
2024-11-15 20:36:42 -0600

    Deleted LikeScreen Tab

--- app/navigation/SessionNavigator.js
+++ app/navigation/SessionNavigator.js
@@ -10,7 +10,6 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import FilterScreen from '../screens/session/tabs/FilterScreen';
 import MatchScreen from '../screens/session/tabs/MatchScreen';
 import AboutScreen from '../screens/session/menu/AboutScreen';
-import LikeScreen from '../screens/session/tabs/LikeScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -23,7 +22,6 @@ function TabNavigator(){
         <Tab.Navigator>
             <Tab.Screen name='Filters' component={FilterScreen} options={{ headerShown: false, headerBlurEffect: 'regular',  }}/>
             <Tab.Screen name='Profiles' component={MatchScreen} options={{ headerShown: false }}/>
-            <Tab.Screen name='Likes' component={LikeScreen} options={{ headerShown: false }}/>
 
         </Tab.Navigator>
 
