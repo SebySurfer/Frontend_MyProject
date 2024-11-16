50459caf50f5e4c50be2d428249ce53ddb8c458a
SebySurfer <sebysurfer@gmail.com>
2024-11-15 18:02:05 -0600

    Blue box start

--- app/screens/session/tabs/FilterScreen.js
+++ app/screens/session/tabs/FilterScreen.js
@@ -1,15 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
 import React from 'react'
 import ColorList from '../../../components/ColorList.js'
 
 
 export default function FilterScreen() {
   return (
-    <View styles={styles.Container}>
-     <ColorList color="#4f46e5"
-
-      />
-    </View>
+    <ScrollView  contentContainerStyle={styles.Container}>
+     <View style={[styles.Boxes, {backgroundColor:"blue"}]}/>
+    </ScrollView>
   )
 }
 
@@ -20,4 +18,16 @@ const styles = StyleSheet.create({
     flexDirection: 'column', 
     gap: 35
   }, 
+
+  Boxes:{
+    width:100, 
+    height: 150, 
+    borderRadius: 25, 
+    borderCurve: 'continuous', 
+    marginBottom: 15, 
+    paddingHorizontal: 20, 
+    paddingVertical: 10, 
+    height:'100%'
+    }
+
 })
